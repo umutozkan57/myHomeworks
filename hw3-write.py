0b3a32fadf29f7806cf888b3eaedbc349408763c
Umut <ozkanumut57@gmail.com>
2022-12-12 22:12:08 +0300

    Updated

--- hw3-write.py
+++ hw3-write.py
@@ -1,2 +1,1 @@
 open("data", "w").write(f'Name: {input("Your Name: ")}\nSurname: {input("Your Surname: ")}\nGender: {input("Your gender: ")}\nUsername: {input("Your username: ")}\nJob: {input("Your job: ")}')
-print({item.split(": ")[0]:item.split(": ")[1] for item in open("data").read().split("\n")})
